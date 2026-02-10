7f0859050c4ea3d33c2cf51a511bb9a6302e8bbc
pkeinde6 <pkeinde6@gmail.com>
2026-02-10 00:34:49 +0000

    deploy

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'todo-list-v1';
+const urlsToCache = [
+  '/todo-list/',
+  '/todo-list/index.html',
+  '/todo-list/manifest.json'
+];
+
+// Installation du Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+// Activation et nettoyage des anciens caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Stratégie de cache : Network First, fallback to cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        // Clone la réponse pour la mettre en cache
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME)
+          .then((cache) => cache.put(event.request, responseClone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
